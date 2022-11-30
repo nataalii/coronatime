123a3f0ae1472bb0a5c4a493b28cd09c80c147e2
nnataali <natalia.charkviani.1@btu.edu.ge>
2022-11-30 13:59:49 +0400

    feat: add translations

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,7 +1,7 @@
 <div>
     <input id="remember_me" type="checkbox" name="remember_me" class="w-5 h-5 rounded border-dark-20 checked:bg-system-success "> 
-    <label for="remember_me" class="ml-2 text-sm font-medium text-dark-100">Remember this device</label>
+    <label for="remember_me" class="ml-2 text-sm font-medium text-dark-100">{{ __('text.remember') }}</label>
   </div>
   <span>
-    <a href="{{ route('password.request') }}" class=" text-brand-primary text-sm font-bold">Forgot password?</a>
+    <a href="{{ route('password.request') }}" class=" text-brand-primary text-sm font-bold">{{ __('text.forgot') }}</a>
 </span>
--- resources/views/register/verify.blade.php
+++ resources/views/register/verify.blade.php
@@ -1,7 +1,12 @@
 @extends('confirmation-layout')
 @section('slot')
 
-<img src="/images/icon-checked.svg" alt="" class="m-auto mt-80">
-<h1 class="text-lg text-dark-100" >We have sent you a confirmation email</h1>
+<img src="/images/icon-checked.svg" alt="" class="m-auto mt-64">
+<h1 class="text-lg text-dark-100" >{{ __('text.confirm_account') }}</h1>
+<div class=" flex w-full mt-24">
+    <button type="submit" class=" h-14 flex w-96 m-auto justify-center content-center rounded-md bg-grn py-2 px-4 font-bold pt-4 text-white shadow-xs focus:outline-none focus:ring-2 ">
+        <a href="{{ route('login.create') }}">{{__("text.signin")  }}</a>
+    </button>
+</div> 
 
 @endsection
