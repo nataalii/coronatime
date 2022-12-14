2f4226b0c5153ea6f510f92507a4ab706d7a93fb
nnataali <natalia.charkviani.1@btu.edu.ge>
2022-12-15 01:59:12 +0400

    fix: add favicon

--- resources/views/confirmation-layout.blade.php
+++ resources/views/confirmation-layout.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="icon" type="image/png" href="{{asset('images/corona-favicon.png') }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
